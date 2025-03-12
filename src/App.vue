8443122e37faab054a7b5284e2ffafb094167a62
gapsofmind <morin.ibrahim@gmail.com>
2025-03-12 14:22:39 +0100

    safe point before SPA

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,36 @@
-  <template>
-    <Logo msg="Project Vite Calender"/>
-    
-    <div>
-      <calendar :fetched-holidays="fetchedHolidays"></calendar>
-      <holidays @fetched-holidays="storeHolidaysArrayData"></holidays> <!-- event listener,methods sends payload to data() -->
-    </div> 
-  
-  </template>
+<template>
+  <Logo msg="Project Vite Calender" />
+
+  <div>About Home And so on</div>
+
+  <div>
+    <calendar :fetched-holidays="fetchedHolidays"></calendar>
+    <holidays @fetched-holidays="storeHolidaysArrayData"></holidays>
+    <!-- event listener,methods sends payload to data() -->
+  </div>
+</template>
 
 <script>
-  import Logo from "./components/Logo.vue";
-  import Calendar from "./components/Calendar.vue";
-  import Holidays from "./components/Holidays.vue";
+import Logo from "./components/Logo.vue";
+import Calendar from "./components/Calendar.vue";
+import Holidays from "./components/Holidays.vue";
 
-  export default {
-    components: {
-      Logo,
-      Calendar,
-      Holidays
+export default {
+  components: {
+    Logo,
+    Calendar,
+    Holidays,
+  },
+  data() {
+    return {
+      fetchedHolidays: null,
+    };
+  },
+  methods: {
+    storeHolidaysArrayData(holidaysArray) {
+      this.fetchedHolidays = holidaysArray;
+      console.log("fetch reached App.vue" + this.fetchedHolidays);
     },
-    data() {
-      return {
-        fetchedHolidays: null
-      };
-    },
-    methods: {
-      storeHolidaysArrayData (holidaysArray) {
-        this.fetchedHolidays = holidaysArray;
-        console.log("fetch reached App.vue" + this.fetchedHolidays)
-      }
-    }
-  };
+  },
+};
 </script>
-
