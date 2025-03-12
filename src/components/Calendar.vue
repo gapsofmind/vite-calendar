025e3473b81d8f7de9fe3dcdc0a9bbabfae86eb9
gapsofmind <morin.ibrahim@gmail.com>
2025-03-12 11:59:43 +0100

    safe point 3/12-25 11.59

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -8,71 +8,69 @@
 </template>
 
 <script>
-import VueCal from 'vue-cal';
-import 'vue-cal/dist/vuecal.css'; 
+import VueCal from "vue-cal";
+import "vue-cal/dist/vuecal.css";
 
 export default {
   components: {
-    VueCal
+    VueCal,
   },
   data() {
     return {
-     events: []
+      events: [],
     };
   },
   props: {
-  fetchedHolidays: {
-    type: Array,
-    default: () => []
-  }
-},
-/* For debugging */
+    fetchedHolidays: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  /* For debugging */
 
-watch: {
+  watch: {
     // Watch for changes in fetchedHolidays
     fetchedHolidays(newVal) {
-      console.log("📩 Prop updated in Calendar.vue:", newVal);
+      console.log("Prop updated in Calendar.vue:", newVal);
 
       // Convert fetched holidays into VueCal event format
-      this.events = newVal.map(holiday => ({
-        start: new Date(holiday.date),  // Ensure date is properly converted
+      this.events = newVal.map((holiday) => ({
+        start: new Date(holiday.date), // Ensure date is properly converted
         end: new Date(holiday.date),
-        title: holiday.name
+        title: holiday.name,
       }));
 
-      console.log("🗓 Events updated in Calendar.vue:", this.events);
-    }
+      console.log("Events updated in Calendar.vue:", this.events);
+    },
   },
 
   methods: {
     handleCellClick(date) {
       const newEvent = {
-        start: date,                // Use the clicked date as the start time
-        end: new Date(date.getTime() + (2 * 60 * 60 * 1000)), //sets end time to 2 hours after start
-        title: 'New Events, 2 hours by default'          // Default title for the new event
+        start: date, // Use the clicked date as the start time
+        end: new Date(date.getTime() + 2 * 60 * 60 * 1000), //sets end time to 2 hours after start
+        title: "New Events, 2 hours by default", // Default title for the new event
       };
-      
+
       // Push the new event to the events array
       this.events.push(newEvent);
 
-      console.log('New event added:', newEvent);
+      console.log("New event added:", newEvent);
     },
-/*     addHolidayToCalendar (holidayDate) {
+    /*     addHolidayToCalendar (holidayDate) {
       const newEvent = {
         start: holidayDate,
         end: holidayDate,
         title: holiday.name
       }
     }, */
-  }
-}
+  },
+};
 </script>
 
-
 <style>
-
 body {
-  background-color: #1C2630;
-  color: rgb(224, 224, 224)
+  background-color: #1c2630;
+  color: rgb(224, 224, 224);
 }
 </style>
--- src/components/Holidays.vue
+++ src/components/Holidays.vue
@@ -1,58 +1,59 @@
-  <template>
-      <h1>Holidays</h1>
-      <div>Enter year and country code:</div>
-      <input v-model="yearForHolidayFetch" placeholder="Year">
-      <input v-model="countryCodeFetch" placeholder="Country Code">
-      <button @click="fetchHolidays">Fetch Holidays</button>
-      <ul>
-        <li v-for="holiday in holidays" :key="holiday.date">
-          {{ holiday.name }} - {{ holiday.localName }} - {{ holiday.date }}
-        </li>
-      </ul>
-    </template>
-    
+<template>
+  <h1>Holidays</h1>
+  <div>Enter year and country code:</div>
+  <div>
+    Clickt button to fetch for {{ yearForHolidayFetch }} and countryode
+    {{ countryCodeFetch }}
+  </div>
+  <input v-model="yearForHolidayFetch" placeholder="Year" />
+  <input v-model="countryCodeFetch" placeholder="Country Code" />
+  <button @click="fetchHolidays">Fetch Holidays</button>
+  <ul>
+    <li v-for="holiday in holidays" :key="holiday.date">
+      {{ holiday.name }} - {{ holiday.localName }} - {{ holiday.date }}
+    </li>
+  </ul>
+</template>
+
 <script>
-  export default {
-    emits: ['fetched-holidays'],
-    
-    data() {
+export default {
+  emits: ["fetched-holidays"],
+
+  data() {
     return {
       holidays: [],
-      yearForHolidayFetch: '2025',  // Default  year
-      countryCodeFetch: 'SE'        // Default country-code
+      yearForHolidayFetch: "2025", // Default  year
+      countryCodeFetch: "SE", // Default country-code
     };
   },
   methods: {
     fetchHolidays() {
       if (this.yearForHolidayFetch && this.countryCodeFetch) {
-        fetch(`https://date.nager.at/api/v3/publicholidays/${this.yearForHolidayFetch}/${this.countryCodeFetch}`)
-          .then(response => response.json())
-          .then(holidaysArray => {
+        fetch(
+          `https://date.nager.at/api/v3/publicholidays/${this.yearForHolidayFetch}/${this.countryCodeFetch}`
+        )
+          .then((response) => response.json())
+          .then((holidaysArray) => {
             this.holidays = holidaysArray; //payload from api to use in this component
-            this.$emit('fetched-holidays', holidaysArray) // custom event and payload to pass on to parent App.vue
-            console.log("methods:fethHolidays" + holidaysArray)
+            this.$emit("fetched-holidays", holidaysArray); // custom event and payload to pass on to parent App.vue
+
+            console.log("methods:fethHolidays" + holidaysArray);
           })
-          .catch(error => console.error('Error fetching holidays:', error));
+          .catch((error) => console.error("Error fetching holidays:", error));
       }
-    }
+    },
   },
 
   mounted() {
     this.fetchHolidays(); // Initial fetch with default values
-  }
+  },
 };
-
-    
-
 </script>
-
-
-
 
 <style>
 li {
   background-color: #3d454d;
   color: white;
-  width: 25%
+  width: 25%;
 }
 </style>
